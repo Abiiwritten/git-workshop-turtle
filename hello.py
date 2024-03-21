19f414659b492f02aba5140ec49df8d6cd9b3d1c
Abhinav Kumar <abhinav.khushal@gmail.com>
2024-03-21 19:48:26 +1000

    add my name to the turtle

--- hello.py
+++ hello.py
@@ -5,4 +5,8 @@ screen = Screen()
 screen.setup(1000, 600)
 write("CODE NETWORK", align="center", font=("Arial", 24, "bold"))
 
+goto(250, 431)
+color("red")
+write("Abhinav Kumar", align="center", font=("papyrus", 14))
+
 done()
